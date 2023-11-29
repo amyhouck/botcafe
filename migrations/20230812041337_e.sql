785ac6175c7ec0a4d1bf511e951a03c0ed49253b
Amelia Houck <145879931+amyhouck@users.noreply.github.com>
2023-11-29 15:32:57 -0500

    Delete migrations/20230812041337_e.sql

--- migrations/20230812041337_e.sql
+++
@@ -1,8 +0,0 @@
--- Add migration script here
-DROP TABLE guild_settings;
-
-CREATE TABLE guild_settings (
-    id INTEGER PRIMARY KEY NOT NULL,
-    guild_id INTEGER NOT NULL,
-    feed_settings_required_roleid INTEGER NOT NULL
-)
